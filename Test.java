1654c09fc2135ed073b7a0b266082b816f286033
pallabojha <ojhapallab@gmail.com>
2017-04-12 16:13:46 +0530

    assingment-3.1
    assingment-3.1

--- Test.java
+++ Test.java
@@ -1,13 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author IIS-KOLKATA
- */
 public class Test {
-    
+    static{
+print(10);
 }
+static void print (int x) {
+System.out.println (x);
+System.exit(0);
+}
+}
+
